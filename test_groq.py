5fc0d6f435830fab8d2ad870195cdd1aa1558cc0
nicolasnascimento-cloud <nicolas.nascimento@sou.unifal-mg.edu.br>
2026-06-28 21:20:39 -0300

    Create test_groq.py

---
+++ test_groq.py
@@ -0,0 +1,11 @@
+from summarizer import gerar_resumo
+
+texto = """
+Petrobras anuncia aumento da produção.
+
+Ibovespa fecha em alta.
+
+Dólar cai frente ao real.
+"""
+
+print(gerar_resumo(texto))
